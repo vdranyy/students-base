532558c2f20632b60c223991edfe52d7732a20bb
Vova Dranyy <v.dranyy@gmail.com>
2016-04-22 10:34:59 +0300

    Extends admin.py, add inline for groups and students

--- base/admin.py
+++ base/admin.py
@@ -2,7 +2,31 @@ from django.contrib import admin
 from .models import Student, Group, Teacher
 
 
-admin.site.register(Student)
-admin.site.register(Group)
-admin.site.register(Teacher)
+class StudentAdmin(admin.ModelAdmin):
+#	date_hierarchy = 'birthday'
+	list_display = ('name', 'birthday', 'student_group')
+	list_filter = ('student_group',)
+	ordering = ('name',)
+	search_fields = ('name',)
+
+
+class StudentInline(admin.TabularInline):
+	model = Student
+
+
+class GroupAdmin(admin.ModelAdmin):
+	inlines = [StudentInline]
+	list_display = ('name', 'praepostor')
+	ordering = ('name',)
+	search_fields = ('name',)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+	list_display = ('name', 'birthday', 'teacher_id', 'user')
+	ordering = ('name',)
+	
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Teacher, TeacherAdmin)
 
